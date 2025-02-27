715faa68250e0e72210f89d567f50b4b5ff5f3f1
PGalcik <pavol.galcik@frequentis.com>
2025-02-27 14:12:20 +0100

    Setup routing

---
+++ src/pages/Layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import NavBar from "../components/NavBar";
+
+const Layout = () => {
+  return (
+    <>
+      <NavBar></NavBar>
+      <Outlet></Outlet>
+    </>
+  );
+};
+
+export default Layout;
