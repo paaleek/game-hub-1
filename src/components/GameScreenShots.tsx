a23fff989b829b92bd466af5e701da1041071062
PGalcik <pavol.galcik@frequentis.com>
2025-02-27 17:48:25 +0100

    Game screenshots

---
+++ src/components/GameScreenShots.tsx
@@ -0,0 +1,25 @@
+import { SimpleGrid, Image } from "@chakra-ui/react";
+import React from "react";
+import useScreenShots from "../hooks/useScreenShots";
+
+interface Props {
+  gameId: number;
+}
+
+const GameScreenShots = ({ gameId }: Props) => {
+  const { data, isLoading, error } = useScreenShots(gameId);
+  console.log(data);
+
+  if (isLoading) return null;
+  if (error) throw error;
+
+  return (
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={2}>
+      {data?.results.map((file) => (
+        <Image key={file.id} src={file.image}></Image>
+      ))}
+    </SimpleGrid>
+  );
+};
+
+export default GameScreenShots;
